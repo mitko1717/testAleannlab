a7420732328515a3969c3f00c8d05c57170a6969
Dmytro Hirich <diman12345677@gmail.com>
2022-01-06 00:23:05 +0200

    Updates

---
+++ precache-manifest.135625257bc96a324359aa151517c2b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff87c90b5ecc25ec5d196f96e65cb6ee",
+    "url": "/testAleannlab/index.html"
+  },
+  {
+    "revision": "4bd63e2f7c3a93971477",
+    "url": "/testAleannlab/static/js/2.044dfd5e.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/testAleannlab/static/js/2.044dfd5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8240600b7edcf37e52e1",
+    "url": "/testAleannlab/static/js/main.580cfeff.chunk.js"
+  },
+  {
+    "revision": "bbd6cd40fea26a62f594",
+    "url": "/testAleannlab/static/js/runtime-main.3e29007d.js"
+  },
+  {
+    "revision": "2204965991bd6112a55ba944a39e4e43",
+    "url": "/testAleannlab/static/media/Inter-Bold.22049659.woff"
+  },
+  {
+    "revision": "e7ae98681edfa1df7f1e3ebba0d4fb88",
+    "url": "/testAleannlab/static/media/Inter-Bold.e7ae9868.woff2"
+  },
+  {
+    "revision": "5ddabe6d5f3898bd2f4bbf2d71e3548e",
+    "url": "/testAleannlab/static/media/Inter-Italic.5ddabe6d.woff"
+  },
+  {
+    "revision": "9ae10ed36ba099d530a76843b39afd9c",
+    "url": "/testAleannlab/static/media/Inter-Italic.9ae10ed3.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/testAleannlab/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "a62973a83e1ae40767014bbe3a542be4",
+    "url": "/testAleannlab/static/media/Inter-Regular.a62973a8.woff2"
+  }
+]);
