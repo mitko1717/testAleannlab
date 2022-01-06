730c3b522d5cc2388d6169056ad29dceecab3c14
Dmytro Hirich <diman12345677@gmail.com>
2022-01-06 05:21:08 +0200

    Updates

--- precache-manifest.134bf433f40d54bc94c25b8e2a047aa8.js
+++ precache-manifest.134bf433f40d54bc94c25b8e2a047aa8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff87c90b5ecc25ec5d196f96e65cb6ee",
+    "revision": "00cb8b2ab9d0e242db1f56d98ccfb8f4",
     "url": "/testAleannlab/index.html"
   },
   {
-    "revision": "4bd63e2f7c3a93971477",
-    "url": "/testAleannlab/static/js/2.044dfd5e.chunk.js"
+    "revision": "25a8bab8f733a4907f33",
+    "url": "/testAleannlab/static/js/2.f5f2c13f.chunk.js"
   },
   {
     "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
-    "url": "/testAleannlab/static/js/2.044dfd5e.chunk.js.LICENSE.txt"
+    "url": "/testAleannlab/static/js/2.f5f2c13f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8240600b7edcf37e52e1",
-    "url": "/testAleannlab/static/js/main.580cfeff.chunk.js"
+    "revision": "2d5453dc0b625157fe86",
+    "url": "/testAleannlab/static/js/main.388e59a4.chunk.js"
   },
   {
     "revision": "bbd6cd40fea26a62f594",
